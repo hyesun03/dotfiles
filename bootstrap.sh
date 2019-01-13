4e9e1aff0465eb2b6195ec90ac8a19fd8be02ace
hyesun03 <chsun0303@gmail.com>
2019-01-13 23:30:24 +0900

    Command Line Tools 설치 스크립트와 echo문 추가

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 
+############################
+##### INSTALL homebrew #####
+############################
+
+echo "===== INSTALL homebrew if not installed ====="
 if test ! $(command -v brew); then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
@@ -18,9 +23,8 @@ packages=(
     pyenv-virtualenv
 )
 
-
+echo "===== INSTALL packages with homebrew ====="
 # brew install "${packages[@]}"
-
 for package in "${packages[@]}"; do
     package_name=$( echo "$package" | awk '{print $1}' )
     if brew list "$package_name" > /dev/null 2>&1; then
@@ -29,4 +33,14 @@ for package in "${packages[@]}"; do
         brew install "$package"
     fi
 done
+
+
+######################################
+##### INSTALL Command Line Tools #####
+######################################
+
+echo "===== INSTALL Command Line Tools if not installed ====="
+if ! command -v xcodebuild > /dev/null; then
+    xcode-select --install
+fi
 
